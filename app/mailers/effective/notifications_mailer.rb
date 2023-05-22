d81a0f2540eed5067fb928cc557396030edb3b4e
Matt Riemer <matthew@codeandeffect.com>
2023-05-22 15:20:44 -0600

    add enabled, csv reports

--- app/mailers/effective/notifications_mailer.rb
+++ app/mailers/effective/notifications_mailer.rb
@@ -2,25 +2,28 @@ module Effective
   class NotificationsMailer < EffectiveMessaging.parent_mailer_class
     include EffectiveMailer
 
-    def notify(notification, opts = {})
-      raise('expected an Effective::Notification') unless notification.kind_of?(Effective::Notification)
+    # def notify(notification, opts = {})
+    #   raise('expected an Effective::Notification') unless notification.kind_of?(Effective::Notification)
 
-      # Returns a Hash of params to pass to mail()
-      # Includes a :to, :from, etc
-      rendered = notification.render_email()
+    #   # Returns a Hash of params to pass to mail()
+    #   # Includes a :to, :from, etc
+    #   rendered = notification.render_email()
 
-      # Works with effective_logging to associate this email with the notification
-      headers = headers_for(notification, opts)
+    #   # Attach report
+    #   attach_report!(notification)
 
-      # Use postmark broadcast-stream
-      headers.merge!(message_stream: 'broadcast-stream') if defined?(Postmark)
+    #   # Works with effective_logging to associate this email with the notification
+    #   headers = headers_for(notification, opts)
 
-      # Calls effective_resources subject proc, so we can prepend [LETTERS]
-      subject = subject_for(__method__, rendered.fetch(:subject), notification, opts)
+    #   # Use postmark broadcast-stream
+    #   headers.merge!(message_stream: 'broadcast-stream') if defined?(Postmark)
 
-      # Pass everything to mail
-      mail(rendered.merge(headers).merge(subject: subject))
-    end
+    #   # Calls effective_resources subject proc, so we can prepend [LETTERS]
+    #   subject = subject_for(__method__, rendered.fetch(:subject), notification, opts)
+
+    #   # Pass everything to mail
+    #   mail(rendered.merge(headers).merge(subject: subject))
+    # end
 
     # Does not use effective_email_templates mailer
     def notify_resource(notification, resource, opts = {})
@@ -30,6 +33,9 @@ module Effective
       # Returns a Hash of params to pass to mail()
       # Includes a :to, :from, etc
       rendered = notification.render_email(resource)
+
+      # Attach report
+      attach_report!(notification)
 
       # Works with effective_logging to associate this email with the notification
       headers = headers_for(notification, opts)
@@ -46,8 +52,28 @@ module Effective
 
     private
 
+    def attach_report!(notification)
+      return unless notification.attach_report?
+
+      report = notification.report
+      raise("expected a report for notification id=#{notification.id}") unless report.present?
+
+      # Attach Report CSV built from datatables
+      datatable = EffectiveReportDatatable.new(view_context, report: report)
+
+      attachments["#{Time.zone.now.strftime('%F')}-#{report.to_s.parameterize}.csv"] = {
+        mime_type: datatable.csv_content_type,
+        content: datatable.csv_file
+      }
+    end
+
     def mailer_settings
       EffectiveMessaging
+    end
+
+    # Authorization for the Datatables.
+    def authorize!(action, resource)
+      true
     end
 
   end
